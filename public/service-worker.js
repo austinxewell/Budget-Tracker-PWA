de0310fb7e5fd1806e5db1af291af9defd66ae59
AustinxEwell <austin.ewell86@gmail.com>
2021-11-26 12:48:39 -0700

    created offline capabilities. BudgetTracker is storing in cache storage.

---
+++ public/service-worker.js
@@ -0,0 +1,16 @@
+const APP_PREFIX = 'BudgetTracker-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    "./index.html"
+  ];
+
+  self.addEventListener('install', function (e) {
+    e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(FILES_TO_CACHE)
+      })
+    )
+  })
